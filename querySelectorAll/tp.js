f197f2cbaa875b157b668f8c6279d23e0d931bb7
bgangware <68155775+bgangware@users.noreply.github.com>
2020-07-25 13:00:18 -0400

    Initial Code

--- querySelectorAll/tp.js
+++ querySelectorAll/tp.js
@@ -1,8 +1,11 @@
 window.setTimeout(function() {
-alert("Hello");
+//alert("Hello");
 
+var h3Var = document.querySelectorAll(".test");
 
+console.log(h3Var);
 
-
+h3Var[0].style.color = "green";
+h3Var[1].style.color = "orange";
 
 }, 500);

782ec145ad920800a6756c455c0c13a0a5f67a2c
bgangware <68155775+bgangware@users.noreply.github.com>
2020-07-25 20:24:43 -0400

    Set up querySelectorAll Examples

--- querySelectorAll/tp.js
+++ querySelectorAll/tp.js
@@ -2,10 +2,26 @@ window.setTimeout(function() {
 //alert("Hello");
 
 var h3Var = document.querySelectorAll(".test");
+var pAllVar = document.querySelectorAll(".allLines");
+var pOddVar = document.querySelectorAll(".oddLines");
+var pEvenVar = document.querySelectorAll(".evenLines");
 
 console.log(h3Var);
 
 h3Var[0].style.color = "green";
 h3Var[1].style.color = "orange";
+
+pAllVar[0].style.background = "yellow";
+pAllVar[1].style.background = "yellow";
+pAllVar[2].style.background = "yellow";
+pAllVar[3].style.background = "yellow";
+pAllVar[4].style.background = "yellow";
+
+pOddVar[0].style.color = "red";
+pOddVar[1].style.color = "red";
+pOddVar[2].style.color = "red";
+
+pEvenVar[0].style.color = "blue";
+pEvenVar[1].style.color = "purple";
 
 }, 500);

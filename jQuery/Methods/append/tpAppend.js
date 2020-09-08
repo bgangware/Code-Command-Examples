9a20bafba5729bf00d74c33249c018bd85afcdfb
bgangware <68155775+bgangware@users.noreply.github.com>
2020-09-08 13:32:07 -0400

    Adding append command

---
+++ jQuery/Methods/append/tpAppend.js
@@ -0,0 +1,11 @@
+window.setTimeout(function() {
+alert("Hello Brand");
+$("h1").text("jQuery is Working"); 
+
+// This is an example of append.  This code is adding to div boxes to two existing boxes.
+$( ".box" ).append( "<div class='container item1'>div 3</div>" );
+$( ".box" ).append( "<div class='container item1'>div 4</div>" );
+
+
+
+}, 500);
